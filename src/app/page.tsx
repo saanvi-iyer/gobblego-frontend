d62991a96107623027d411a834a6ec02fcaca962
saanvi-iyer <saanvi.iyer007@gmail.com>
2025-03-27 00:42:42 +0530

    fix: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,21 @@
 "use client";
 import Image from "next/image";
-import { Macondo_Swash_Caps } from "next/font/google";
-
-const macondoSwashCaps = Macondo_Swash_Caps({
-  subsets: ["latin"],
-  weight: "400",
-});
 
 export default function Home() {
   return (
-    <div className="h-screen bg-black flex flex-col justify-end">
-      <p
-        className={`absolute ${macondoSwashCaps.className} text-6xl text-white `}
+    <div className="h-screen bg-black flex flex-col justify-end relative">
+      <div
+        className={`absolute font-mancondo text-6xl text-white w-full h-full flex items-center justify-center mb-48`}
       >
-        ikigai
-      </p>
-      <div className="w-full h-full">
-        <Image
-          src="/bg.svg"
-          alt=""
-          className="h-fit w-auto bg-white"
-          width={1000}
-          height={1000}
-        />
+        GobbleGo
       </div>
+      <Image
+        src="/bg.svg"
+        alt="alt"
+        className="h-fit w-auto"
+        width={1000}
+        height={1000}
+      />
     </div>
   );
 }
