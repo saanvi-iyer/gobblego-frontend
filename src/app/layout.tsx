3caa4bccbbd049fcc3ee839f868b244088fbcaa2
saanvi-iyer <saanvi.iyer007@gmail.com>
2025-03-27 01:28:43 +0530

    feat: menu page filters

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${macondoSwashCaps.className} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${macondoSwashCaps.variable} antialiased bg-black`}
       >
         {children}
       </body>
---
+++ src/app/menu/types.ts
@@ -0,0 +1,10 @@
+export interface MenuResponse {
+  item_id: string;
+  item_name: string;
+  price: number;
+  is_available: boolean;
+  category: string;
+  est_prep_time: number;
+  description: string;
+  image?: string;
+}
