34a5126610db8dfe3ea4c4c70effe69c03fb1ec8
saanvi-iyer <saanvi.iyer007@gmail.com>
2025-03-30 18:23:47 +0530

    fix: home page redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 "use client";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-black flex flex-col justify-end">
-      <div className="absolute text-6xl text-white w-full h-full flex items-center justify-center mb-48 font-mancondo">
-        GobbleGo
-      </div>
-      <Image
-        src="/bg.svg"
-        alt="alt"
-        className="h-fit w-auto"
-        width={1000}
-        height={1000}
+    <div className="min-h-screen bg-black flex flex-col justify-end relative">
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/bg.svg')" }}
       />
+      <div className="absolute text-[#FFA050] w-full h-full mb-4 flex flex-col items-center justify-center gap-y-6">
+        <p className="font-giaza text-7xl">gobblego</p>
+        <div className="text-[#b99a7e] font-semibold text-lg tracking-widest">Scan. Order. Repeat.</div>
+      </div>
+      
     </div>
   );
 }
