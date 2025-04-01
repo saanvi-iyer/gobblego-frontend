00cf0941875bc4390d4bc427c0f4174f4005df69
saanvi-iyer <saanvi.iyer007@gmail.com>
2025-04-01 23:48:31 +0530

    style: ui fixes

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { User2, Funnel } from "lucide-react";
+import { User2, Funnel, ShoppingCart } from "lucide-react";
 import axios, { AxiosError } from "axios";
 import { MenuResponse } from "./types";
 import { MenuItem } from "./menu-item";
+import Link from "next/link";
 
 function MenuComponent() {
   const [menuItems, setMenuItems] = useState<MenuResponse[]>([]);
@@ -30,10 +31,16 @@ function MenuComponent() {
   return (
     <div className="text-white min-h-screen">
       <div className="p-4">
-        <User2 />
-        <p className="text-3xl font-bold">Menu</p>
+        <div className="text-3xl font-bold flex flex-row items-center justify-between">
+          <p>Menu</p>
+          <div className="flex flex-row gap-4">
+            <User2 />
+            <Link href="/cart">
+              <ShoppingCart />
+            </Link>
+          </div>
+        </div>
         <div className="flex items-center gap-4 mb-6">
-          <Funnel />
           <div className="flex overflow-x-auto pb-2">
             <button
               className={`py-2 text-white relative transition-all duration-300 
@@ -51,7 +58,7 @@ function MenuComponent() {
             {categories.map((category) => (
               <button
                 key={category}
-                className={`px-4 py-2 text-white relative transition-all duration-300 
+                className={`px-4 py-2 text-white relative transition-all duration-300  whitespace-nowrap
                   ${
                     activeCategory === category
                       ? "after:bg-[#D68037]"
